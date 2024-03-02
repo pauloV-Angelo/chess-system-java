cae77f342f993e8212b8522a0a4c52988344f441
Paulo Vitor <angelopaulovitor@gmail.com>
2024-03-02 18:24:12 -0300

    Teste de envio de outro pc

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,10 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		for(int i = 0;i<10;i++) {
+			System.out.println("Hello World!");
+		}
 
 	}
 
